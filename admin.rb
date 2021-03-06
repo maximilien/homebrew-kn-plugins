dfc60a0f43109b1de872e56b58c482d01e2b7ed8
dr.max <maximilien@acm.org>
2021-03-05 16:26:32 -0800

    formulas for kn plugins admin and source-kafka

---
+++ admin.rb
@@ -0,0 +1,36 @@
+if OS.mac?
+  require "FileUtils"
+else
+  require "fileutils"
+end
+
+class Admin < Formula
+  v = "v0.21.0"
+  plugin_name = "kn-plugin-admin"
+
+  homepage "https://github.com/knative-sandbox/#{plugin_name}"
+  base_url = "https://github.com/knative-sandbox/#{plugin_name}/releases/download/#{v}"
+
+  version v
+
+  if OS.mac?
+    url "#{base_url}/#{plugin_name}-darwin-amd64"
+    sha256 "9a10e1b0ff9a035d88b6d69d5d7bf235de8a2b4b1002062688e104fe936f4161"
+  else
+    url "#{base_url}/#{plugin_name}-linux-amd64"
+    sha256 "05badf881ac18f7fb4d567b995d98f480a080ed5e64728882392af65db28dfa6"
+  end
+
+  def install
+    if OS.mac?
+      FileUtils.mv("#{plugin_name}-darwin-amd64", plugin_name)
+    else
+      FileUtils.mv("#{plugin_name}-linux-amd64", plugin_name)
+    end
+    bin.install plugin_name
+  end
+
+  test do
+    system "#{bin}/#{plugin_name}", "version"
+  end
+end
---
+++ source-kafka.rb
@@ -0,0 +1,36 @@
+if OS.mac?
+  require "FileUtils"
+else
+  require "fileutils"
+end
+
+class SourceKafka < Formula
+  v = "v0.21.0"
+  plugin_name = "kn-plugin-source-kafka"
+
+  homepage "https://github.com/knative-sandbox/#{plugin_name}"
+  base_url = "https://github.com/knative-sandbox/#{plugin_name}/releases/download/#{v}"
+
+  version v
+
+  if OS.mac?
+    url "#{base_url}/#{plugin_name}-darwin-amd64"
+    sha256 "62009d94134abb23f208b725ba47eb5da5ca8bf55a554300576cbc595f111069"
+  else
+    url "#{base_url}/#{plugin_name}-linux-amd64"
+    sha256 "ac1f39208094b74f728dd71e4073b0dcee7f96fc18da3e2ab88ca4e482508695"
+  end
+
+  def install
+    if OS.mac?
+      FileUtils.mv("#{plugin_name}-darwin-amd64", plugin_name)
+    else
+      FileUtils.mv("#{plugin_name}-linux-amd64", plugin_name)
+    end
+    bin.install plugin_name
+  end
+
+  test do
+    system "#{bin}/#{plugin_name}", "version"
+  end
+end
